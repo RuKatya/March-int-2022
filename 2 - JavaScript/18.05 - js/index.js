8c59345f74fa745153c52fe18bbfb3a8734a3cf6
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-18 21:28:11 +0300

    input

---
+++ 2 - JavaScript/18.05 - js/index.js
@@ -0,0 +1,30 @@
+// const - משתנה קבוע
+// let - משתנה ניתן לשינוי
+// boolean - true / false
+// string
+
+//NaN - Not a number
+// console.log(typeof Number('1'))
+// console.log(Number('a'))
+
+//null - לא קיים
+//undefiend - לא נמצא
+//console.log(x)
+
+////////////
+// alert() - חלונית קופצת - רק אוקיי
+// const q = prompt("are you okey?") //string
+
+// const q = +prompt("are you okey?") //number
+// console.log(typeof q)
+
+
+// if (q === 'yes') {
+//     alert('good')
+// }
+
+// confirm() - only true/false
+//////////////
+//Use prompt() to read a first name and a last name.
+//Declare the variable fullName, and then welcome the user by his full name.
+
